3045769b20b27e00e3480986e9015c7a6709fee9
perkabem-mpw <perkabem@gmail.com>
2026-06-30 23:47:24 +0700

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -1,1 +1,38 @@
 
+let stream = null;
+
+async function startCamera() {
+
+  try {
+
+    stream = await navigator.mediaDevices.getUserMedia({
+
+      video: {
+        facingMode: {
+          ideal: "environment"
+        }
+      },
+
+      audio: false
+
+    });
+
+    const video = document.getElementById("video");
+
+    video.srcObject = stream;
+
+    await video.play();
+
+    console.log("✅ Camera Started");
+
+  } catch (err) {
+
+    console.error(err);
+
+    alert("Camera Error : " + err.message);
+
+  }
+
+}
+
+window.onload = startCamera;
