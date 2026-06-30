5e95b17ec85306d34b6261323cfc64f2de770c39
perkabem-mpw <perkabem@gmail.com>
2026-06-30 16:21:15 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,30 @@
+const API_URL =
+"https://script.google.com/macros/s/AKfycbzk78w5BqDWSPOmCsNJe_QfMwVvqhsFD0HLe4ypCb0zt3SEDbF-RvvZyw1tkrLDWWXolQ/exec?page=api";
 
+async function previewMember(memberId){
+
+    const res = await fetch(
+
+        API_URL +
+        "&action=preview&memberId=" +
+        encodeURIComponent(memberId)
+
+    );
+
+    return await res.json();
+
+}
+
+async function checkInMember(memberId){
+
+    const res = await fetch(
+
+        API_URL +
+        "&action=checkin&memberId=" +
+        encodeURIComponent(memberId)
+
+    );
+
+    return await res.json();
+
+}
