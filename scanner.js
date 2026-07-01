a6d7807d72ac5240c66e41f0668fc80b395dd488
perkabem-mpw <perkabem@gmail.com>
2026-07-01 10:00:25 +0700

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -2,47 +2,30 @@ let codeReader;
 
 async function startScanner(){
 
+    codeReader = new ZXingBrowser.BrowserQRCodeReader();
+
     try{
 
-        const video = document.getElementById("video");
+        await codeReader.decodeFromConstraints(
 
-        codeReader = new ZXingBrowser.BrowserQRCodeReader();
+            {
+                video:{
+                    facingMode:"environment"
+                }
 
-        const devices =
-            await ZXingBrowser.BrowserCodeReader.listVideoInputDevices();
+            },
 
-        console.log(devices);
-
-        let deviceId = devices[0].deviceId;
-
-        // pilih kamera belakang kalau ada
-        devices.forEach(function(d){
-
-            const name = d.label.toLowerCase();
-
-            if(
-                name.includes("back") ||
-                name.includes("rear") ||
-                name.includes("environment")
-            ){
-
-                deviceId = d.deviceId;
-
-            }
-
-        });
-
-        codeReader.decodeFromVideoDevice(
-
-            deviceId,
-
-            video,
+            "video",
 
             (result,error)=>{
 
                 if(result){
 
-                    onDetected(result.text);
+                    console.log(result.text);
+
+                    alert("QR : "+result.text);
+
+                    codeReader.stop();
 
                 }
 
@@ -57,25 +40,6 @@ async function startScanner(){
         alert(err);
 
     }
-
-}
-
-let lastCode = "";
-
-function onDetected(text){
-
-    if(text==lastCode){
-        return;
-    }
-
-    lastCode=text;
-
-    console.log("QR =",text);
-
-    alert("QR : "+text);
-
-    // sementara berhenti dulu
-    codeReader.stop();
 
 }
 
